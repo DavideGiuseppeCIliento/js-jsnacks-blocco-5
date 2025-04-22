48c1a7995152f0eed96cf46228c4625ac9dd0793
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-22 15:13:01 +0200

    Eser 1 terminato.

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,13 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const stampaNomi = () => {
+  for (let i = 0; i < names.length; i++) {
+    let currentName = names[i];
+    console.log(currentName);
+  }
+};
+
+stampaNomi();
