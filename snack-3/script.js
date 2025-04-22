82a73a4d9271b4c89e81aa880005bce73997f97a
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-22 17:40:18 +0200

    + Codice di Samuel

--- snack-3/script.js
+++ snack-3/script.js
@@ -7,8 +7,39 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // console.log(newArray);
 
-const CreateNewArray = () => {
-  return numbers.map((numero) => (numero += 1));
-};
+function myMapFilter(array, funzione) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    const current = array[i];
+    const newCurrent = funzione(current, i); // altro valore
+    if (newCurrent !== undefined) {
+      result.push(newCurrent);
+    }
+  }
+  return result;
+}
 
-console.log(CreateNewArray());
+function myFilter(array, funzione) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    const current = array[i];
+    const daInserire = funzione(current, i); // bool
+    if (daInserire) {
+      result.push(current);
+    }
+  }
+  return result;
+}
+
+const arr = [1, 5, 7, 2, 0];
+
+const newArray = myMapFilter(arr, function (current, index) {
+  if (index % 2 !== 0) {
+    // dispari
+    return undefined;
+  } else {
+    return current + 1;
+  }
+}); // [2, 6, 8, 3, 1]
+
+console.log(newArray);
